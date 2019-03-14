e68d5c53e81a39f27bbb3772b329fccd8c64a556
Alex Sasser, Patrick Miller and Rohan Straker <alex+patrick+rohan@>
2019-03-13 20:12:22 -0700

    add business and user logic to javascript.

--- README.md
+++ README.md
@@ -1,16 +1,9 @@
-# Ping Pong
-# by Patrick Miller
+# Address Book
 
-### A web application that takes a number from a user and returns a range of numbers from 1 to the chosen number with the following exceptions:
+### A web application that creates address book entries using a form and javascript logic
 
 
 #### Specifications
-
-* Numbers divisible by 3 are replaced with "ping"
-* Numbers divisible by 5 are replaced with "pong"
-* Numbers divisible by 15 are replaced with "pingpong"
-* A user should be able to enter a new number and see new results over and over again.
+*
 ---
-Copyright - Patrick Miller 2019. No license is required to use this code. And no attribution is necessary.
----
-Site hosted on Github pages: https://patmillertime.github.io/pingpong
+Copyright - Patrick Miller, Alex Sasser, Rohan Straker 2019. No license is required to use this code. And no attribution is necessary.
--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,85 @@
-$(document).ready(function(event) {
-    $("form#pingpong").submit(function(event) {
-    $('#output').text("");
+// Business Logic for AddressBook ---------
+function AddressBook() {
+  this.contacts = []
+}
+
+AddressBook.prototype.addContact = function(contact) {
+  this.contacts.push(contact);
+}
+
+// Business Logic for Contacts ---------
+function Contact(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+  this.addresses = [];
+}
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city = city;
+  this.state = state;
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + " " + this.state;
+}
+//user Logic
+$(document).ready(function() {
+
+  $("#add-address").click(function() {
+    $("#new-addresses").append('<div class="new-address">' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-street">Street</label>' +
+                                   '<input type="text" class="form-control new-street">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-city">City</label>' +
+                                   '<input type="text" class="form-control new-city">' +
+                                 '</div>' +
+                                 '<div class="form-group">' +
+                                   '<label for="new-state">State</label>' +
+                                   '<input type="text" class="form-control new-state">' +
+                                 '</div>' +
+                               '</div>');
+  });
+
+  $("form#new-contact").submit(function(event) {
     event.preventDefault();
 
-        var value;
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
 
-    		function pingPong(){
+    $(".new-address").each(function() {
+      var inputtedStreet = $(this).find("input.new-street").val();
+      var inputtedCity = $(this).find("input.new-city").val();
+      var inputtedState = $(this).find("input.new-state").val();
+      var newAddress = new Address(inputtedStreet, inputtedCity, inputtedState)
+      newContact.addresses.push(newAddress)
+    });
 
-    			for (var i = 1; i <= $('#input1').val(); i++) {
-    				if (i % 15 == 0) {
-    					value = 'PingPong'
-    				}
-    				else if (i % 3 == 0) {
-    					value = 'Ping'
-    				}
-    				else if (i % 5 == 0) {
-    					value = 'Pong'
-    				}
-    				else {
-    					value = i
-    				}
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
-    				$('#output').append('<li>' + value + '</li>')
-    			}
-    		}
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.fullName());
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+      $("ul#addresses").text("");
+      newContact.addresses.forEach(function(address) {
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
+      });
+    });
 
-    		$(document).ready(function(){
-    			pingPong();
-    		})
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+
   });
 });
